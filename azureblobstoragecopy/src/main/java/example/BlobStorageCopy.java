fd7513878f330fea3d365c52d417ddafe5d570d5
alfonsof <alfonsofb@gmail.com>
2022-01-04 19:49:04 +0100

    Updated Azure SDK version and refactoring

--- azureblobstoragecopy/src/main/java/example/BlobStorageCopy.java
+++ azureblobstoragecopy/src/main/java/example/BlobStorageCopy.java
@@ -1,7 +1,6 @@
 /**
  * BlobStorageCopy is an example that handles Blob Storage containers on Microsoft Azure.
  * Copy a Blob from a Blob Storage container to another Blob Storage container.
- * The credentials are taken from AZURE_AUTH_LOCATION environment variable.
  * The connection string is taken from app.properties file.
  * You must use 3 parameters:
  * SOURCE_CONTAINER      = Source container name
@@ -14,20 +13,21 @@ package example;
 import java.io.InputStream;
 import java.util.Properties;
 import java.io.IOException;
-import com.microsoft.azure.storage.CloudStorageAccount;
-import com.microsoft.azure.storage.StorageException;
-import com.microsoft.azure.storage.blob.CloudBlobClient;
-import com.microsoft.azure.storage.blob.CloudBlobContainer;
-import com.microsoft.azure.storage.blob.CloudBlob;
-import com.microsoft.azure.storage.blob.CloudBlockBlob;
-import com.microsoft.azure.storage.blob.CopyStatus;
+import java.time.OffsetDateTime;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.sas.BlobSasPermission;
+import com.azure.storage.blob.sas.BlobServiceSasSignatureValues;
+
 
 public class BlobStorageCopy {
     public static void main(String[] args) {
         String sourceContainerName;      // Source container name
-        String sourceBlobName;           // Source blob
+        String sourceBlobName;           // Source blob name
         String destinationContainerName; // Destination container name
-        String destinationBlobName;      // Destination blob
+        String destinationBlobName;      // Destination blob name
 
         if (args.length < 3) {
             System.out.println("Not enough parameters.\nProper Usage is: java -jar azureblobstoragecopy.jar <SOURCE_CONTAINER> <SOURCE_BLOB> <DESTINATION_CONTAINER>");
@@ -46,41 +46,63 @@ public class BlobStorageCopy {
         // Load Configuration from a file and get the Storage Connection String
         String storageConnectionString = loadConfiguration();
 
-        try
-        {
-            // Retrieve storage account from connection-string.
-            CloudStorageAccount storageAccount = CloudStorageAccount.parse(storageConnectionString);
+        // Copy a Blob from a Blob Storage container to another Blob Storage container.
+        copyBlob(storageConnectionString, sourceContainerName, sourceBlobName, destinationContainerName);
+    }
 
-            // Create the blob client.
-            CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+    /**
+     * Load Configuration from a file and get the Storage Connection String.
+     */
+    private static String loadConfiguration() {
 
-            // Get a reference to a source container.
-            // The container name must be lower case
-            CloudBlobContainer sourceContainer = blobClient.getContainerReference(sourceContainerName);
+        // The connection string is taken from app.properties file
+        Properties prop = new Properties();
 
-            if (sourceContainer.exists()) {
-                // Get a reference to source Blob.
-                CloudBlockBlob sourceBlob = sourceContainer.getBlockBlobReference(sourceBlobName);
+        try {
+            InputStream is = ClassLoader.getSystemResourceAsStream("app.properties");
+            prop.load(is);
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+        String storageAccountConnectionString = prop.getProperty("StorageAccountConnectionString");
 
-                if (sourceBlob.exists()) {
-                    // Get a reference to a destiny container.
-                    // The container name must be lower case
-                    CloudBlobContainer destinationContainer = blobClient.getContainerReference(destinationContainerName);
+        return storageAccountConnectionString;
+    }
 
-                    if (destinationContainer.exists()) {
-                        // Get a reference to destiny Blob.
-                        CloudBlockBlob destinationBlob = destinationContainer.getBlockBlobReference(destinationBlobName);
+    /**
+     * Copy a Blob from a Blob Storage container to another Blob Storage container.
+     */
+    private static void copyBlob(String storageConnectionString,
+                                 String sourceContainerName,
+                                 String sourceBlobName,
+                                 String destinationContainerName) {
+        try {
+            // Create a BlobServiceClient object which will be used to create a container client
+            BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(storageConnectionString).buildClient();
+
+            // Get the source container client object
+            BlobContainerClient sourceContainerClient = blobServiceClient.getBlobContainerClient(sourceContainerName);
+
+            if (sourceContainerClient.exists()) {
+                // Get a reference to the source blob
+                BlobClient sourceBlobClient = sourceContainerClient.getBlobClient(sourceBlobName);
+                if (sourceBlobClient.exists()) {
+                    // Get the destination container client object
+                    BlobContainerClient destinationContainerClient = blobServiceClient.getBlobContainerClient(destinationContainerName);
+                    if (destinationContainerClient.exists()) {
+                        // Get a reference to the destination blob
+                        BlobClient destinationBlobClient = destinationContainerClient.getBlobClient(sourceBlobName);
+
+                        System.out.println(String.format("\nCopying Blob: \"%s\"", sourceBlobClient.getBlobUrl()));
+                        System.out.println(String.format("from container: \"%s\"", sourceContainerClient.getBlobContainerUrl()));
+                        System.out.println(String.format("to container: \"%s\"", destinationContainerClient.getBlobContainerUrl()));
+
+                        // Create the SAS Token to get the permission to access the source blob
+                        String sasToken = generateSasToken(sourceBlobClient);
+
                         // Copy the blob
-                        System.out.println(String.format("\nCopying Blob: \"%s\".", sourceBlob.getUri().toURL()));
-                        System.out.println(String.format("to container: \"%s\".", destinationContainer.getUri().toURL()));
-                        destinationBlob.startCopy(sourceBlob);
-
-                        if (waitForCopyToComplete(destinationBlob) == CopyStatus.SUCCESS) {
-                            System.out.printf("Blob \"%s\" copied\n", sourceBlobName);
-                        } else {
-                            System.out.printf("Error: Blob \"%s\" NOT copied.\n", sourceBlobName);
-                        }
-
+                        destinationBlobClient.copyFromUrl(sourceBlobClient.getBlobUrl() + "?" + sasToken);
+                        System.out.println("Copied");
                     } else {
                         System.out.printf("Error: Destination Blob Storage container \"%s\" does NOT exist.\n", destinationContainerName);
                     }
@@ -90,59 +112,23 @@ public class BlobStorageCopy {
             } else {
                 System.out.printf("Error: Source Blob Storage container \"%s\" does NOT exist.\n", sourceContainerName);
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             // Output the stack trace.
             e.printStackTrace();
         }
     }
 
     /**
-     * Wait until the copy is completed.
-     *
-     * @param blob Target of the copy operation
-     *
-     * @throws InterruptedException
-     * @throws StorageException
+     * Create the SAS Token to get the permission to access a blob.
      */
-    private static CopyStatus waitForCopyToComplete(CloudBlob blob) throws InterruptedException, StorageException {
-        CopyStatus copyStatus = CopyStatus.PENDING;
-        while (copyStatus == CopyStatus.PENDING) {
-            Thread.sleep(1000);
-            blob.downloadAttributes();
-            copyStatus = blob.getCopyState().getStatus();
-        }
+    private static String generateSasToken(BlobClient blobClient) {
+        OffsetDateTime expiryTime = OffsetDateTime.now().plusDays(1);
+        BlobSasPermission permission = new BlobSasPermission().setReadPermission(true);
 
-        return copyStatus;
-    }
+        BlobServiceSasSignatureValues values = new BlobServiceSasSignatureValues(expiryTime, permission)
+                                                            .setStartTime(OffsetDateTime.now());
+        String sasToken = blobClient.generateSas(values);
 
-    /**
-    * Load Configuration from a file and get the Storage Connection String
-    */
-    private static String loadConfiguration() {
-
-        // The connection string is taken from app.properties file
-        Properties prop = new Properties();
-
-        try {
-            InputStream is = ClassLoader.getSystemResourceAsStream("app.properties");
-            prop.load(is);
-        } catch(IOException e) {
-            System.out.println(e.toString());
-        }
-        String defaultEndpointsProtocolStr = prop.getProperty("DefaultEndpointsProtocol");
-        String accountNameStr = prop.getProperty("AccountName");
-        String accountKeyStr = prop.getProperty("AccountKey");
-        String endpointSuffixStr = prop.getProperty("EndpointSuffix");
-
-        // Define the connection-string with your values
-        String storageConnectionString =
-                "DefaultEndpointsProtocol=" + defaultEndpointsProtocolStr + ";" +
-                        "AccountName=" + accountNameStr + ";" +
-                        "AccountKey="+ accountKeyStr + ";" +
-                        "EndpointSuffix="+ endpointSuffixStr;
-
-        return storageConnectionString;
+        return sasToken;
     }
 }
