119e6ca2528b5dfd6abb026151e4d767b2022d37
alfonsof <alfonsofb@gmail.com>
2022-08-25 23:25:04 +0200

    Refactor Function.java, pom.xml & host.json

--- azurefunctionblobmove/src/main/java/example/Function.java
+++ azurefunctionblobmove/src/main/java/example/Function.java
@@ -1,79 +1,96 @@
 package example;
 
-import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import com.microsoft.azure.functions.ExecutionContext;
-import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.OutputBinding;
 import com.microsoft.azure.functions.annotation.FunctionName;
-import com.microsoft.azure.functions.annotation.StorageAccount;
 import com.microsoft.azure.functions.annotation.BlobTrigger;
 import com.microsoft.azure.functions.annotation.BindingName;
-import com.microsoft.azure.storage.StorageException;
-import com.microsoft.azure.storage.CloudStorageAccount;
-import com.microsoft.azure.storage.blob.CloudBlobClient;
-import com.microsoft.azure.storage.blob.CloudBlobContainer;
-import com.microsoft.azure.storage.blob.CloudBlob;
-import com.microsoft.azure.storage.blob.BlobContainerPublicAccessType;
+import com.microsoft.azure.functions.annotation.BlobOutput;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobClient;
 
 
 /**
- * Azure Functions with Azure Blob trigger.
+ * Azure Functions with Azure Blob trigger and move a Blob.
  */
 public class Function {
     /**
-     * This function will be invoked when a new or updated blob is detected at the specified path. The blob contents are provided as input to this function.
+     * This function will be invoked when a new or updated blob is detected at the specified path.
+     * The blob contents are provided as input to this function.
      * Move the blob when it appears in a blob storage to another blob storage.
      */
-    @FunctionName("Function")
-    @StorageAccount("<connection>")
+
+    static final String sourceContainerName = "samples-workitems";
+
+    @FunctionName("BlobMove")
     public void run(
-        @BlobTrigger(connection = "MY_STORAGE_IN", name = "content", path = "samples-workitems/{name}", dataType = "binary") byte[] content,
-        @BindingName("name") String name,
-        final ExecutionContext context
-    ) {
-        context.getLogger().info("Java Blob trigger function processed a blob.\nBlob Name: samples-workitems/" + name + "\nBlob Size: " + content.length + " Bytes");
-        
-        CloudStorageAccount sourceStorageAccount;
-        CloudBlobClient sourceBlobClient = null;
-        CloudBlobContainer sourceContainer = null;
-        CloudBlob sourceBlob = null;
+            @BlobTrigger(
+                connection = "MY_STORAGE_IN",
+                name = "inputblob",
+                path = "samples-workitems/{blobName}",
+                dataType = "binary")
+                byte[] content,
+            @BindingName("blobName") String blobName,
+            @BlobOutput(
+                connection = "MY_STORAGE_OUT",
+                name = "outputblob",
+                path = "samples-workitems/{blobName}")
+                OutputBinding<String> outputData,
+            final ExecutionContext context) {
 
-        CloudStorageAccount destinationStorageAccount;
-        CloudBlobClient destinationBlobClient = null;
-        CloudBlobContainer destinationContainer = null;
-        CloudBlob destinationBlob = null;
-        
-        // Get Storage Connection String of thesource storage account
+        context.getLogger().info("Java Blob trigger function processed a blob.\nBlob Name: "
+                                + sourceContainerName + "/" + blobName + "\nBlob Size: " + content.length + " Bytes");
+
+        // Get Storage Connection String of the source storage account
         String sourceStorageConnectionString = System.getenv("MY_STORAGE_IN");
-
-        // Get Storage Connection String of the destination storage account
-        String destinationStorageConnectionString = System.getenv("MY_STORAGE_OUT");
-
-        try {
-            // Parse the connection string and create a blob client to interact with source Blob storage
-            sourceStorageAccount = CloudStorageAccount.parse(sourceStorageConnectionString);
-            sourceBlobClient = sourceStorageAccount.createCloudBlobClient();
-            sourceContainer = sourceBlobClient.getContainerReference("samples-workitems");
-            sourceBlob = sourceContainer.getBlockBlobReference(name);
-
-            // Parse the connection string and create a blob client to interact with destination Blob storage
-            destinationStorageAccount = CloudStorageAccount.parse(destinationStorageConnectionString);
-            destinationBlobClient = destinationStorageAccount.createCloudBlobClient();
-            destinationContainer = destinationBlobClient.getContainerReference("samples-workitems");
-            destinationBlob = destinationContainer.getBlockBlobReference(name);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        
         try {
             // Copy the blob
-            context.getLogger().info("Moving blob: " + name);
-            destinationBlob.uploadFromByteArray(content, 0, content.length);
-            // Delete the source blob
-            sourceBlob.deleteIfExists();
+            context.getLogger().info("Moving blob: " + blobName);
+            
+            // Save blob to outputData
+            outputData.setValue(new String(content, StandardCharsets.UTF_8));
+
+            // Delete the source Blob
+            deleteBlob(sourceStorageConnectionString, sourceContainerName, blobName, context);
+
             context.getLogger().info("Moved");
-        } catch (StorageException se) {
-            se.printStackTrace();
-        } catch (IOException e) {
+        } catch (Exception e) {
+            // Output the stack trace.
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Delete a Blob in a Blob Storage container.
+     */
+    private static void deleteBlob(String storageConnectionString, String containerName, String blobName,
+                                    ExecutionContext context) {
+        try {
+
+            // Create a BlobServiceClient object which will be used to create a container client
+            BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(storageConnectionString).buildClient();
+
+            // Get the container client object
+            BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
+
+            if (containerClient.exists()) {
+                // Get the blob client object
+                BlobClient blobClient = containerClient.getBlobClient(blobName);
+                if (blobClient.exists()) {
+                    // Delete the blob
+                    blobClient.delete();
+                } else {
+                    context.getLogger().info("Error: Blob \"%s\" does NOT exist.\n" + blobName);
+                }
+            } else {
+                context.getLogger().info("Error: Blob Storage container \"%s\" does NOT exist.\n" + containerName);
+            }
+        } catch (Exception e) {
+            // Output the stack trace.
             e.printStackTrace();
         }
     }
